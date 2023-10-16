00e9bc214ea5b4add59a96453afd70c51fb85c81
Half-Gray <halfgray7e@gmail.com>
2023-10-16 11:11:53 -0700

    Use relative paths for redirections

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-$isHttps = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off';
-$baseLocation = ($isHttps ? 'https' : 'http') . '://' . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']) . '/';
 if (!empty($_GET['r'])) {
 	header('HTTP/1.1 301 Moved Permanently');
-	header('Location: ' . $baseLocation . 'releases/' . rawurlencode($_GET['r']) . '/');
+	header('Location: ./releases/' . rawurlencode($_GET['r']) . '/');
 } else {
 	readfile('index.html');
 }
--- wipa/wips.php
+++ wipa/wips.php
@@ -1,11 +1,9 @@
 <?php
-$isHttps = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off';
-$baseLocation = ($isHttps ? 'https' : 'http') . '://' . $_SERVER['SERVER_NAME'] . dirname(dirname($_SERVER['REQUEST_URI'])) . '/';
 if (!empty($_GET['r'])) {
 	header('HTTP/1.1 301 Moved Permanently');
-	header('Location: ' . $baseLocation . 'releases/' . rawurlencode($_GET['r']) . '/#ref-list-wips');
+	header('Location: ../releases/' . rawurlencode($_GET['r']) . '/#ref-list-wips');
 } else {
 	header('HTTP/1.1 302 Found');
-	header('Location: ' . $baseLocation . 'wipa/');
+	header('Location: ./wipa/');
 }
 ?>
